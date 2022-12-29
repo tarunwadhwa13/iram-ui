63184aab2a3d31dc83395e1d6a4e71ea94b3c95c
Tarun Wadhwa <tarunwadhwa13@gmail.com>
2022-12-29 12:15:26 +0530

    upgrade dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,12 +5,9 @@ module.exports = {
   },
   extends: [
     'plugin:vue/vue3-essential',
-    '@vue/standard',
-    '@vue/typescript/recommended'
+    'eslint:recommended',
+    '@vue/typescript'
   ],
-  parserOptions: {
-    ecmaVersion: 2020
-  },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
